b60f99b42d586c2368fbb58b2268716637175656
wao218 <wao218@lehigh.edu>
2014-09-03 02:10:49 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////////////////////
+//Wesley Osborne
+//hw01
+//Welcome Class Java Program 
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+
+public class WelcomeClass
+{
+    
+//  add main method
+    public static void main(String[] args) {
+
+//  print the statement with box around it
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-W--A--O--2--1--8-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v   ");
+    System.out.println("I am from Boston,MA and I have nine siblings");
+    
+    //test/testtest
+  }
+}
