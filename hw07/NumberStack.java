b1629b8a1e8f362a7a2756fe8ecec9ec698f2f69
wao218 <wao218@lehigh.edu>
2014-10-22 02:44:28 +0000

    Updated NumberStack

---
+++ hw07/NumberStack.java
@@ -0,0 +1,185 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//Wesley Osborne
+//hw07
+//NumberStack Program
+//October 20, 2014
+//CSE2
+//
+/*a program that forces the user to enter an integer between 1 and 9, inclusive, 
+and then prints out displays that depend on the value entered and stacks the numbers.*/
+
+//Import Scanner
+import java.util.Scanner;
+
+//Define Class
+public class NumberStack {
+
+    //Add main method
+    public static void main(String[] args) {
+
+            //Declare instance of scanner to accpet user input
+            Scanner input;
+            //Call Scanner Constructor
+            input = new Scanner(System.in);
+
+            System.out.print("Enter an integer between 1 and 9: ");
+
+
+            int x = input.nextInt(); //input from user
+
+            //check for integers between 1 and 9 inclusive
+            if (x >= 1 && x <= 9) {
+
+                //Creates separates groups
+                for (int groups = 1; groups < x + 1; groups++) {
+
+                    //Creats pattern for each line
+                    for (int lines = 0; lines < groups; lines++) {
+
+                        //Creates spacing for pyramid shape for numbers
+                        for (int spaces = 0; spaces < (8 - groups); spaces++) {
+                            System.out.print(" "); //prints out spacing
+                        } //end of loop
+
+                        //Increments numbers by 1 up to input entered
+                        for (int numbers = 0; numbers < ((2 * groups) - 1); numbers++) {
+
+                            System.out.print(groups); //prints out numbers
+
+                        } //end of loop
+                        System.out.println(); //prints out spaces between each group
+                    } //end of loop
+
+                    //Creates spacing for pyramid shape for dashes
+                    for (int lines = 0; lines < (8 - groups); lines++) {
+                        System.out.print(" "); //prints out spacing
+                    } //end of loop
+
+                    //Increments dashes by 1 up to input entered
+                    for (int numbers = 0; numbers < ((2 * groups) - 1); numbers++) {
+                        System.out.print("-"); //prints out dashes 
+                    } //end of loop
+                    System.out.println(""); //prints out space between groups
+                } //end of loop 
+
+            } //end of if statement
+            else {
+                System.out.println("You did not enter an interger between 1 and 9"); //prints out if input is not between 1 and 9 inclusive
+            }//end of else statement
+
+            //check for integers between 1 and 9 inclusive
+            if (x >= 1 && x <= 9) {
+                int groups = 1;
+                int lines = 0;
+                int spaces = 0;
+                int numbers = 0;
+
+                //Creates separates groups
+                groups = 0;
+                while (groups < x) {
+                    groups++;
+
+                    //Creates pattern for each line
+                    lines = 0;
+                    while (lines < groups) {
+                        lines++;
+
+                        //Creates spacing for pyramid shape for numbers
+                        spaces = 0;
+                        while (spaces < (9 - groups)) {
+                            System.out.print(" "); //prints out spacing
+                            spaces++;
+                        } //end of loop
+
+                        //Increments numbers by 1 up to input entered
+                        numbers = 0;
+                        while (numbers < ((2 * groups) - 1)) {
+
+                            System.out.print(groups); //prints out numbers
+                            numbers++;
+                        } //end of loop 
+                        System.out.println(); //prints out spaces between each group
+                    } //end of loop 
+
+                    //Creates spacing for pyramid shape for dashes
+                    lines = 0;
+                    while (lines < (9 - groups)) {
+                        lines++;
+                        System.out.print(" "); //prints out spacing
+
+                    } //end of loop 
+
+                    //Increments dashes by 1 up to input entered
+                    numbers = 0;
+                    while (numbers < ((2 * groups) - 1)) {
+                        numbers++;
+                        System.out.print("-"); //prints out dashes
+
+                    } //end of loop 
+                    System.out.println(""); //prints out space between groups   
+                } //end of loop 
+            } //end of if statement 
+            else {
+                System.out.println("You did not enter an interger between 1 and 9"); //prints out if input is not between 1 and 9 inclusive
+            } //end of else statement 
+
+
+            //check for integers between 1 and 9 inclusive
+            if (x >= 1 && x <= 9) {
+                int groups = 1;
+                int lines = 0;
+                int spaces = 0;
+                int numbers = 0;
+
+                //Creates separates groups
+                groups = 1;
+                do {
+
+                    //Creates pattern for each line
+                    lines = 0;
+                    do {
+
+                        //Creates spacing for pyramid shape for numbers
+                        spaces = 0;
+                        do {
+                            System.out.print(" "); //prints out spacing
+                            spaces++;
+                        } while (spaces < (9 - groups)); //end of loop
+
+                        //Increments numbers by 1 up to input entered
+                        numbers = 0;
+                        do {
+                            System.out.print(groups); //prints out numbers
+                            numbers++;
+                        } while (numbers < ((2 * groups) - 1)); //end of loop 
+
+                        System.out.println(); //prints out spaces between each group
+                        lines++;
+
+                    } while (lines < groups); //end of loop
+                    //Creates spacing for pyramid shape for dashes
+                    lines = 0;
+                    do {
+                        lines++;
+                        System.out.print(" "); //prints out spacing
+
+                    } while (lines < (9 - groups)); //end of loop 
+
+                    //Increments dashes by 1 up to input entered
+                    numbers = 0;
+                    do {
+                        numbers++;
+                        System.out.print("-"); //prints out dashes
+                    } while (numbers < ((2 * groups) - 1)); //end of loop 
+                    System.out.println(""); //prints out space between groups
+                    groups++;
+
+                } while (groups < x + 1); //end of loop 
+
+            } //end of if statement 
+            else {
+                System.out.println("You did not enter an interger between 1 and 9"); //prints out if input is not between 1 and 9 inclusive
+            } //end of else statement
+
+        } //end of main method
+} //end of class
---
+++ lab08/MoreLoops.java
@@ -0,0 +1,152 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//Wesley Osborne
+//lab08
+//Root Program
+//October 17, 2014
+//CSE2
+//
+/*rewrite for loops as while loops, while loops as do-while loops, etc. 
+Below is a program that uses various kinds of loops for various calculations. 
+For the four sets of code, replace the code with code that rewrites the corresponding loops in some other form.*/
+
+import java.util.Scanner;
+public class MoreLoops {
+    public static void main(String[] arg) {
+        Scanner scan = new Scanner(System.in);
+        int n = 0;
+
+        System.out.print("Enter an int- ");
+        //while (!scan.hasNextInt()) {
+        //    scan.next(); //get rid of the junk entered by user
+        //    System.out.print("You did not enter an int; try again- ");
+        //}
+        
+        do{
+            scan.next();
+            System.out.print("You did not enter an int; try again- ");
+        }while(!scan.hasNextInt()); 
+        
+        
+        //COMMENT OUT THE ABOVE 5 LINES AND REPLACE THE CODE WITH A 
+        //DO-WHILE LOOP
+      //  n = scan.nextInt();
+
+        //for (int j = 0; j < n && j < 40; j++) {
+        //    for (int k = 0; k < j + 1; k++) {
+        //        System.out.print('*');
+        //    }
+        //    System.out.println();
+        //}
+        
+        int j=0;
+        int k=0;
+        
+        while(j < n && j < 40 ){
+            
+            while(k < j + 1){
+                System.out.print('*');
+                k++;
+            }
+            k=0;
+            System.out.println();
+            j++;
+        }
+        
+        
+    int j=0;
+        int k=0;
+        
+        while(j < n && j < 40 ){
+            j++;
+            while(k < j){
+                System.out.print('*');
+                k++;
+            }
+            k=0;
+            System.out.println();
+        }
+        
+        
+        //COMMENT OUT THE ABOVE SIX LINES OF CODE AND REPLACE WITH 
+        //WHILE STATEMENTS 
+
+        int k = 4;
+
+        //do {
+        //    System.out.println("k=" + k);
+        //    k++;
+        //}
+        //while (k < 4);
+        
+        System.out.println("k=" + k);
+        while(k < 4) {
+            System.out.println("k=" + k);
+            k++;
+        }
+        
+        
+        
+        //COMMENT OUT THE ABOVE FIVE LINES AND REPLACE THE CODE
+        //WITH A WHILE LOOP
+
+        //int count = 0;
+        //while (true) {
+        //    switch (n) {
+        //        default: System.out.println(n + " is > 5 or <1");
+        //        break;
+        //        case 1:
+        //        case 2:
+        //            System.out.print("Case 2 ");
+        //            continue;
+        //        case 3:
+        //            break;
+        //        case 4:
+        //            System.out.println("Case 4");
+        //        case 5:
+        //            System.out.println("Case 5");
+        //            break;
+        //    }
+        
+        
+        //    count++;
+        //    if (count > 10)
+        //        break;
+        //}
+        int count = 0;
+        for(count=0; count < 11; count++) {
+            for(n = 0; n<6; n++){
+                if (n<1 || n>5){
+                    System.out.println(n + " is > 5 or <1");
+                    break;
+                }
+                if (n==1 || n==2){
+                   System.out.println("Case 2 ");
+                   continue; 
+                }
+                if (n==3){
+                    break;
+                }
+                if (n==4){
+                   System.out.println("Case 4");
+                   System.out.println("Case 5");
+                   break;
+                }
+                if(n==5){
+                    System.out.println("Case 5");
+                    break;
+                }
+                break;
+            }
+        }
+        
+        
+        
+        
+        //COMMENT OUT THE ABOVE 16 LINES AND REWRITE THE CODE UING
+        // ONLY FOR LOOPS AND IF STATEMENS. BEFORE DOING SO, TRY
+        //RUNNING THE CODE WITH VARIOUS INPUTTED VALUES, E.G., 
+        //   -5, 1, 20, 5, ETC.
+        //IF YOU GET AN INFINITE LOOP, YOU CAN STOP IT BY 
+        //SIMULTANEOUSLY PRESSING THE CTRL KEY AND THE C KEY
+    }//End of main method
+}//End of class
